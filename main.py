b8eb0d8d180e1aa3f0461f78cd6de47d848e0c26
Ronald-ICastroG <87490001+Ronald-ICastroG@users.noreply.github.com>
2022-02-07 23:54:38 -0500

    cambios antes de dar push

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 print("""-----Menú de opciones----\n
       ¿qué acción quieres ejecutar?
       1. registrar usuario
@@ -10,14 +9,3 @@ print("""-----Menú de opciones----\n
       """)
 
 opcion=input("Seleccione el # de la opcion: ")
-=======
-print("Bienvenidos al menú de compras")
-print("""
-      ¿Qué desea hacer?
-      1. crear usuario
-      2. Seleccionar usuario
-      3. nueva factura de compras
-      4. historial de compras
-      5. revisión de caja
-      """)
->>>>>>> cabecera
