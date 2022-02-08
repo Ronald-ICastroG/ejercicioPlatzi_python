d6393d48189f6c5de86cd87f3dfc829f1aea3d21
Ronald-ICastroG <87490001+Ronald-ICastroG@users.noreply.github.com>
2022-02-07 23:29:39 -0500

    menú de opciones agregado a main.py, 5 opciones

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Bienvenidos al menú de compras")
+print("""
+      ¿Qué desea hacer?
+      1. crear usuario
+      2. Seleccionar usuario
+      3. nueva factura de compras
+      4. historial de compras
+      5. revisión de caja
+      """)
