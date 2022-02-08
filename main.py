3bd011794f10fed30033182197cb98ee5a7ad5f1
Ronald-ICastroG <87490001+Ronald-ICastroG@users.noreply.github.com>
2022-02-07 23:32:41 -0500

    Creación de menú, 6 opciones en main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("""-----Menú de opciones----\n
+      ¿qué acción quieres ejecutar?
+      1. registrar usuario
+      2. actualizar datos
+      3. crear factura
+      4. mostrar flujo de caja
+      5. mostrar inventario
+      6. salir\n
+      """)
